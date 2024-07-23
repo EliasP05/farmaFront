923068b1c8b3aefef6c50f0d984d105d0caccdca
Elias05 <eliasalberto0505@gmail.com>
2024-07-23 11:05:49 -0300

    boton comparr

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const sideMenu=document.querySelector("aside");
 const menuBtn=document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
 const themeToggler= document.querySelector('.theme-toggler');
+const comparaBtn=document.querySelector("#button");
 
 
 menuBtn.addEventListener('click',()=>{
@@ -20,3 +21,13 @@ themeToggler.addEventListener('click',()=>{
     themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
     themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
 })
+//boton comparacion
+var spinner=document.querySelector('.spinner');
+var text=document.querySelector(".btn-text")
+comparaBtn.addEventListener('click',()=>{
+    text.style.display='none';
+    spinner.style.display= 'block';
+
+
+})
+
